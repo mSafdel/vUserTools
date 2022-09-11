bdf6c3509784e24b5d8a52e1c6214397aaf3f8d1
Mohammad Safdel <mSafdel@gmail.com>
2022-09-11 16:06:52 +0430

    Version 1.0

---
+++ frmMain.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace vUserTools
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.colUserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colUserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnUnlock = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(20, 19);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtSearch.Size = new System.Drawing.Size(602, 35);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkRed;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colUserName,
+            this.colUserId});
+            this.dgvUsers.EnableHeadersVisualStyles = false;
+            this.dgvUsers.Location = new System.Drawing.Point(20, 64);
+            this.dgvUsers.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvUsers.MultiSelect = false;
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgvUsers.RowHeadersVisible = false;
+            this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsers.ShowEditingIcon = false;
+            this.dgvUsers.Size = new System.Drawing.Size(602, 439);
+            this.dgvUsers.TabIndex = 2;
+            // 
+            // colUserName
+            // 
+            this.colUserName.FillWeight = 350F;
+            this.colUserName.HeaderText = "نام و نام خانوادگی";
+            this.colUserName.Name = "colUserName";
+            this.colUserName.ReadOnly = true;
+            this.colUserName.Width = 350;
+            // 
+            // colUserId
+            // 
+            this.colUserId.FillWeight = 250F;
+            this.colUserId.HeaderText = "نام کاربری";
+            this.colUserId.Name = "colUserId";
+            this.colUserId.ReadOnly = true;
+            this.colUserId.Width = 250;
+            // 
+            // btnUnlock
+            // 
+            this.btnUnlock.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnlock.Location = new System.Drawing.Point(20, 520);
+            this.btnUnlock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(222, 62);
+            this.btnUnlock.TabIndex = 3;
+            this.btnUnlock.Text = "Unlock";
+            this.btnUnlock.UseVisualStyleBackColor = true;
+            this.btnUnlock.Click += new System.EventHandler(this.btnUnlock_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(292, 507);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 91);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "mSafdel";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 607);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUnlock);
+            this.Controls.Add(this.dgvUsers);
+            this.Controls.Add(this.txtSearch);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "vUserTools";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Button btnUnlock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUserId;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Diagnostics;
+
+namespace vUserTools
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                PrincipalContext userActiveDirecory = new PrincipalContext(ContextType.Domain, "...Your Domain Name...");
+                UserPrincipal userName = new UserPrincipal(userActiveDirecory);
+                PrincipalSearcher searchObj = new PrincipalSearcher(userName);
+
+                foreach (UserPrincipal result in searchObj.FindAll())
+                {
+                    if (result.DisplayName != null)
+                    {
+                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+
+                        if (IsActive(de))
+                        {
+                            string strUserName = result.DisplayName;
+                            string strUserId = de.Properties["samAccountName"].Value.ToString();
+                            dgvUsers.Rows.Add(strUserName, strUserId);
+                        }
+                    }
+                }
+
+                dgvUsers.Sort(dgvUsers.Columns["colUserId"], ListSortDirection.Ascending);
+                searchObj.Dispose();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searchValue = txtSearch.Text.Trim().ToLower();
+            int iSearchLen = searchValue.Length;
+
+            //Debug.Print(iSearchLen.ToString());
+            try
+            {
+                foreach (DataGridViewRow row in dgvUsers.Rows)
+                {
+                    string user = row.Cells[1].Value.ToString().ToLower();
+                    //Debug.Print(user + " - " + user.Length);
+
+                    if (user.Length >= iSearchLen)
+                    {
+                        if (user.Substring(0, iSearchLen).Equals(searchValue))
+                        {
+                            row.Selected = true;
+                            dgvUsers.FirstDisplayedScrollingRowIndex = dgvUsers.SelectedRows[0].Index;
+                            break;
+                        }
+                        else
+                            dgvUsers.ClearSelection();
+
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnUnlock_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                 if (dgvUsers.SelectedRows != null)
+                {
+                    Debug.Print(dgvUsers.SelectedRows[0].Cells[1].ToString());
+                    string strSelectedUser = dgvUsers.SelectedRows[0].Cells[1].Value.ToString();
+
+                    PrincipalContext userActiveDirecory = new PrincipalContext(ContextType.Domain, "...Your Domain Name...");
+                    UserPrincipal user = UserPrincipal.FindByIdentity(userActiveDirecory, IdentityType.SamAccountName, strSelectedUser);
+
+                    if (user != null)
+                    {
+                        Debug.Print(user.IsAccountLockedOut().ToString());
+                        if (user.IsAccountLockedOut())
+                        {
+                            user.UnlockAccount();
+                            MessageBox.Show("Account is Unlock!","vUserTools",MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private bool IsActive(DirectoryEntry de)
+        {
+            if (de.NativeGuid == null) return false;
+
+            int flags = (int)de.Properties["userAccountControl"].Value;
+
+            return !Convert.ToBoolean(flags & 0x0002);
+        }
+    }
+}
